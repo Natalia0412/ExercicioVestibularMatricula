d3510e7fdff4980bb9be7edd0796503f00daf2a8
Natália Barros <natalia.vieiramoneiro70@gamil.com>
2022-06-03 22:09:20 -0300

    Redux

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const prompts = require('prompts');
 const redux=require('redux');
 //criadora de ação para realizar o vestibular
 const realizarVestibular = (nome,cpf)=> {
@@ -42,3 +43,35 @@ const todosDoReducers=redux.combineReducers({
 })
 
 const store=redux.createStore(todosDoReducers)
+
+const main= async()=>{
+    const menu="1-Realizar Vestibular\n2-Realizar Matricula\n3-Visualizar meu status\n4-Visualizar a lista de aprovados\n0-Sair"
+    let responselike
+    do{
+        response=await prompts({
+            type:"number",
+            name:'op',
+            message:menu
+        })
+        switch(response.op){
+            case 1:
+                //1 pegar nome do usuario 
+                const {nome}= await prompts({typetype:'text',name:'nome',message:'Digite seu nome'})
+                //2 pegar o cpf do usuario
+                const {cpf}=await prompts({typetype:'text',name:'cpf',message:'Digite seu cpf'})
+                //3 construir uma acao apropriada
+                const {acao}= realizarVestibular(nome,cpf)
+                //4 fazer dispatch da acao
+                store.dispatch(acao)
+                break;
+            case 2:
+                //1 pegar o cpf com prompts
+                const {cpf2}=prompts({typetype:'text',name:'cpf',message:'Digite seu cpf'})
+                //2Checar se a pessoa esta aprovada no estado centralizado(store.getState(). HistoricoVestibular.find)
+                const aprovado=store.getState().historicoVestibular.find(aluno=>aluno.cpf===cpf&&aluno.nota>=6)
+                store.dispatch(realizarMatricula(cpf,aprovado?"M":"NM"))                
+                //3 Produzir uma acao apropriada: o status poderá ser M ou NM
+                break;
+        }
+    }while(response.op!=0)
+}
